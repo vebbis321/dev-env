b2ca63d8e4bdaa780a7cbda51f9bb690fddc360c
thel <57636432+vebbis321@users.noreply.github.com>
2025-07-11 13:56:32 +0200

    started on ipython flow for nvim

--- env/.config/nvim/lua/config/iPython-flow.lua
+++ env/.config/nvim/lua/config/iPython-flow.lua
@@ -1,5 +1,16 @@
-vim.keymap.set("n", "<leader>ip", function()
-	vim.cmd.new()
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+
+local job_id = 0
+local function ipython_init()
+	vim.cmd.vnew()
 	vim.cmd.term()
-	vim.cmd.windcmd("L")
-end)
+	vim.cmd.wincmd("L")
+	vim.api.nvim_win_set_width(0, 45)
+
+	job_id = vim.bo.channel
+	vim.fn.chansend(job_id, { "ipython\r\n" })
+end
+
+vim.keymap.set("n", "<leader>ip", ipython_init())
+
+vim.keymap.set("n", "<leader>sp")
