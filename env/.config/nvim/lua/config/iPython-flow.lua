57c13f15b36eeb75abc47ccd744f2e626ff62e80
thel <57636432+vebbis321@users.noreply.github.com>
2025-07-11 14:28:18 +0200

    simple ipython flow

--- env/.config/nvim/lua/config/iPython-flow.lua
+++ env/.config/nvim/lua/config/iPython-flow.lua
@@ -11,6 +11,45 @@ local function ipython_init()
 	vim.fn.chansend(job_id, { "ipython\r\n" })
 end
 
-vim.keymap.set("n", "<leader>ip", ipython_init())
+vim.keymap.set("n", "<leader>ip", ipython_init)
 
-vim.keymap.set("n", "<leader>sp")
+local function send_line_or_visual()
+	local mode = vim.fn.mode()
+	local bufnr = vim.api.nvim_get_current_buf()
+
+	if mode == "v" or mode == "V" or mode == "" then
+		-- Visual mode: get selection
+		local start_pos = vim.fn.getpos("'<")
+		local end_pos = vim.fn.getpos("'>")
+		local lines = vim.api.nvim_buf_get_lines(bufnr, start_pos[2] - 1, end_pos[2], false)
+		for _, line in ipairs(lines) do
+			vim.fn.chansend(job_id, { line .. "\r\n" })
+		end
+	else
+		-- Normal mode: send current line
+		local line = vim.api.nvim_get_current_line()
+		vim.fn.chansend(job_id, { line .. "\r\n" })
+	end
+end
+
+local function send_paragraph()
+	local start = vim.fn.search("^\\s*$", "bnW") + 1
+	local finish = vim.fn.search("^\\s*$", "nW") - 1
+	local bufnr = vim.api.nvim_get_current_buf()
+
+	if start == 0 then
+		start = 1
+	end
+	if finish == -2 then
+		finish = vim.api.nvim_buf_line_count(bufnr)
+	end
+
+	local lines = vim.api.nvim_buf_get_lines(bufnr, start - 1, finish, false)
+
+	for _, line in ipairs(lines) do
+		vim.fn.chansend(job_id, { line .. "\r\n" })
+	end
+end
+
+vim.keymap.set({ "n", "v" }, "<leader>sc", send_line_or_visual)
+vim.keymap.set({ "n", "v" }, "<leader>sp", send_paragraph)
